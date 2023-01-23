7111945b420ca9424931d264de57a0274f4f9e3c
Aks Dev <AksDev@gmail.com>
2023-01-23 23:50:43 +0530

    Added multiple keys for parsing name as API is inconsistent.

--- NewsArticles/Module/Articles/Model/Article.swift
+++ NewsArticles/Module/Articles/Model/Article.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Aks_dev on 04/11/22.
 //
+// swiftlint:disable identifier_name
 
 struct ArticleResponse: Decodable {
     let articles: [Article]
@@ -18,5 +19,19 @@ struct Article: Decodable {
 }
 
 struct Source: Decodable {
-    let name: String
+    let name: String?
+    
+    enum CodingKeys: String, CodingKey, CaseIterable {
+        case name, Name
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        if let key = container.allKeys.filter({ CodingKeys.allCases.contains($0) }).first,
+            let ids = try container.decodeIfPresent(String.self, forKey: key) {
+                self.name = ids
+            } else {
+                self.name = ""
+        }
+    }
 }
